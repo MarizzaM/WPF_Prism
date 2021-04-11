59d2eca16ab4c028810a7473f36bda5a976a7979
Pavlova Marina <marizza.mil89@gmail.com>
2021-04-11 20:29:17 +0300

    Add project files.

---
+++ WPF_Lesson_11_04_21/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF_Lesson_11_04_21
+{
+    public class MainWindowViewModel
+    {
+        public DelegateCommand DelegateCommand1 { get; set; }
+        public DelegateCommand DelegateCommand2 { get; set; }
+
+        public MainWindowViewModel()
+        {
+
+            DelegateCommand1 = new DelegateCommand(
+            () => DelegateCommand2.RaiseCanExecuteChanged());
+
+            DelegateCommand2 = new DelegateCommand(
+                () => MessageBox.Show("delegate command"),
+                () =>
+                {
+                    return DateTime.Now.Second % 2 == 0;
+                });
+
+        }
+
+
+    }
+}
